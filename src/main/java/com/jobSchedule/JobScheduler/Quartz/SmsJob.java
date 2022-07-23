5137fdbba56a3538b36f5ec9c2e8caa84cc48815
MohamedGrari <graristar@gmail.com>
2022-07-23 09:47:03 +0100

    enhance EventHandler.onPersist logic

--- src/main/java/com/jobSchedule/JobScheduler/Quartz/SmsJob.java
+++ src/main/java/com/jobSchedule/JobScheduler/Quartz/SmsJob.java
@@ -7,23 +7,20 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class SmsJob extends QuartzJobBean {
     @Autowired
     EmployerService employerService;
-    private final String ACCOUNT_SID = "ACa70269fa83571dd0bfb400dd1f5ec734";
-    private final String AUTH_TOKEN = "536d201ef6ba879817268bfc0d77f996";
-    private final String FROM = "+14782161663";
+    private static final String ACCOUNT_SID = "ACa70269fa83571dd0bfb400dd1f5ec734";
+    private static final String AUTH_TOKEN = "536d201ef6ba879817268bfc0d77f996";
+    private static final String FROM = "+14782161663";
     private static final Logger logger = LoggerFactory.getLogger(EmailJob.class);
     @Override
     protected void executeInternal(JobExecutionContext context){
